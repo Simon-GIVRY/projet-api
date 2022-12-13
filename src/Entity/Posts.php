8c1ab8e8841740c1437d40c21dc89de5f87d4a53
Simon-GIVRY <simongivry.sg@gmail.com>
2022-12-13 16:04:08 +0100

    projet-api

---
+++ src/Entity/Posts.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\PostsRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+#[ORM\Entity(repositoryClass: PostsRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Delete()
+    ],
+    normalizationContext:["groups" => ["Post:read"]],
+    denormalizationContext:["groups" => ["Post:write"]]
+
+)]
+#[ApiFilter(SearchFilter::class, properties:["titles" => "partial", "description" => "partial"])]
+#[ApiFilter(BooleanFilter::class, properties:["isPublished"])]
+class Posts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(["Post:read", "Post:write"])]
+    private ?string $TextContent = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(["Post:read", "Post:write"])]
+    private ?string $urlLinkedMedia = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(["Post:read", "Post:write"])]
+    private ?string $tags = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups("Post:read")]
+    private ?int $likes = null;
+
+    #[ORM\Column]
+    #[Groups("Post:read")]
+    private ?bool $isPublished = false;
+
+    #[ORM\ManyToOne(targetEntity:"User", inversedBy: 'posts',cascade: ["persist"])]
+    #[ORM\JoinColumn(nullable: false)]
+    #[NotBlank()]
+    #[Groups(["Post:read", "Post:write", "User:read"])]
+    private ?User $postOwner = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getTextContent(): ?string
+    {
+        return $this->TextContent;
+    }
+
+    public function setTextContent(?string $TextContent): self
+    {
+        $this->TextContent = $TextContent;
+
+        return $this;
+    }
+
+    public function getUrlLinkedMedia(): ?string
+    {
+        return $this->urlLinkedMedia;
+    }
+
+    public function setUrlLinkedMedia(?string $urlLinkedMedia): self
+    {
+        $this->urlLinkedMedia = $urlLinkedMedia;
+
+        return $this;
+    }
+
+    public function getTags(): ?string
+    {
+        return $this->tags;
+    }
+
+    public function setTags(?string $tags): self
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    public function getLikes(): ?int
+    {
+        return $this->likes;
+    }
+
+    public function setLikes(?int $likes): self
+    {
+        $this->likes = $likes;
+
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getPostOwner(): ?User
+    {
+        return $this->postOwner;
+    }
+
+    public function setPostOwner(?User $postOwner): self
+    {
+        $this->postOwner = $postOwner;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext:[]),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Delete()
+    ],
+    normalizationContext:[ "groups" => ["User:read"]],
+    denormalizationContext:[ "groups" => ["User:write"]]
+)]
+#[ApiFilter(SearchFilter::class, properties:["titles" => "partial", "description" => "partial"])]
+#[UniqueEntity(fields:['username'])]
+
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    #[Groups(["User:read", "User:write", "Post:read", "Post:write"])]
+    #[NotBlank()]
+    private ?string $username = null;
+
+    #[ORM\Column]
+    #[Groups(["User:read", "Post:read", "Post:write"])]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    #[Groups(["User:write", "Post:write"])]
+    private ?string $password = null;
+
+    #[Groups(["User:read", "User:write", "Post:read", "Post:write"])]
+    #[ORM\Column(length: 255)]
+    #[NotBlank()]
+    private ?string $email = null;
+
+    #[Groups(["User:read", "Post:read"])]
+    #[ORM\OneToMany(mappedBy: 'postOwner', targetEntity: Posts::class)]
+    private Collection $posts;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Posts>
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Posts $post): self
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts->add($post);
+            $post->setPostOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Posts $post): self
+    {
+        if ($this->posts->removeElement($post)) {
+            // set the owning side to null (unless already changed)
+            if ($post->getPostOwner() === $this) {
+                $post->setPostOwner(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
